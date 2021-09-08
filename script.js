7cb0ffde02a9b29cb0a8d4569b6c48a7888e980d
Paulo Victor Saraiva <pvsaraivs@hotmail.com>
2021-09-08 20:59:36 -0300

    Teste de funcionalidade

--- script.js
+++ script.js
@@ -1,29 +1,63 @@
-fetch('https://jsonplaceholder.typicode.com/posts/1')
-  .then(response => response.json())
-  .then(json => {
-    const div = document.getElementById("posts");
-    div.innerText = JSON.stringify(json.title);
-
-})
-
-function request() {
-    fetch("https://jsonplaceholder.typicode.com/posts")
-    .then(function(response){
-        return response.json()
-    })
-    .then((response) => {
-        var result = document.getElementById('result')
-        var item = ''
-        console.log(response)
-        response.forEach(element => {
-            item = 'User Id:' + element.userId + "Id:" + element.id + "title:" + element.title + "completed:" + element.completed + "<br><br>"
-            
-            console.log(item)
-
-            result.append(item)
-
-        });
-       
-        
-    })
+function toggleDiv() {
+  if(document.getElementById("exemplo1").style.display == 'none') {
+      document.getElementById("exemplo1").style.display = 'block';
+        document.getElementById("exemplo2").style.display = 'none';
+  } else {
+    document.getElementById("exemplo1").style.display = 'none';
+      document.getElementById("exemplo2").style.display = 'block';
+  } 
 }
+
+function toggleDiv2() {
+  if(document.getElementById("exemplo1").style.display == 'none') {
+      document.getElementById("exemplo1").style.display = 'block';
+        document.getElementById("exemplo3").style.display = 'none';
+  } else {
+    document.getElementById("exemplo1").style.display = 'none';
+      document.getElementById("exemplo3").style.display = 'block';
+  } 
+}
+
+function toggleDiv3() {
+  if(document.getElementById("exemplo1").style.display == 'none') {
+      document.getElementById("exemplo1").style.display = 'block';
+        document.getElementById("exemplo4").style.display = 'none';
+  } else {
+    document.getElementById("exemplo1").style.display = 'none';
+      document.getElementById("exemplo4").style.display = 'block';
+  } 
+}
+
+function toggleDiv4() {
+  if(document.getElementById("exemplo2").style.display == 'none') {
+      document.getElementById("exemplo2").style.display = 'block';
+        document.getElementById("exemplo1").style.display = 'none';
+  } else {
+    document.getElementById("exemplo2").style.display = 'none';
+      document.getElementById("exemplo1").style.display = 'block';
+  } 
+}
+
+function toggleDiv5() {
+  if(document.getElementById("exemplo3").style.display == 'none') {
+      document.getElementById("exemplo3").style.display = 'block';
+        document.getElementById("exemplo1").style.display = 'none';
+  } else {
+    document.getElementById("exemplo3").style.display = 'none';
+      document.getElementById("exemplo1").style.display = 'block';
+  } 
+}
+
+function toggleDiv6() {
+  if(document.getElementById("exemplo4").style.display == 'none') {
+      document.getElementById("exemplo4").style.display = 'block';
+        document.getElementById("exemplo1").style.display = 'none';
+  } else {
+    document.getElementById("exemplo4").style.display = 'none';
+      document.getElementById("exemplo1").style.display = 'block';
+  } 
+}
+
+fetch('https://jsonplaceholder.typicode.com/todos/1')
+  .then(response => response.json())
+  .then(json => console.log(json))
